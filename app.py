8db9ed29614df4817c453535c37e9b584a682f85
kabiryusufbashir <kabiryusufbashir@gmail.com>
2025-08-29 17:18:32 +0100

    Type Conversion

---
+++ app.py
@@ -0,0 +1,7 @@
+
+
+# 3) Type Conversion 
+# first_num = input('Enter the first number: ')
+# second_num = input('Enter the second number: ')
+# sum = float(first_num) + float(second_num)
+# print(sum)
