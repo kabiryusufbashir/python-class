b03df55a0a3e0465b0ac79a23aa19fbd954a5983
kabiryusufbashir <kabiryusufbashir@gmail.com>
2025-08-29 18:24:51 +0100

    Logical, Comparison operators

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+# 5) Weight Convert 
+# weight = float(input('Enter your weight: '))
+# weight_scale = input('(K)g or (L)bs: ')
+
+# if weight_scale == 'K' or weight_scale == 'k':
+#     converted = weight * 0.45
+#     print("Weight in Kg: "+str(converted))
+# elif weight_scale == 'L' or weight_scale == 'l':
+#     converted = weight / 0.45
+#     print("Weight in bs: "+str(converted))
+# else:
+#     print('Please enter a valid scale...')
+
 # 4) Srings 
-course = 'Python for Beginners'
-print('Python' in course)
+# course = 'Python for Beginners'
+# print('Python' in course)
 
 # 3) Type Conversion 
 # first_num = input('Enter the first number: ')
